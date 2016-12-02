641443227ee95caaa4c8ec33186694fd9171ea17
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-12-02 11:07:04 +0100

    Initial commit for development

--- UIKit/io/guill/UIKit/tests/UIColorTest.java
+++ UIKit/io/guill/UIKit/tests/UIColorTest.java
@@ -10,6 +10,8 @@ public class UIColorTest {
 	@Test
 	public void alphaTest() {
 		assertEquals(128, new UIColor(1,1,1,0.5).getAlpha());
+		assertEquals(255, new UIColor("#FFFFF").getAlpha());
+		assertEquals(255, new UIColor(1,1,1).getAlpha());
 	}
 
 }
